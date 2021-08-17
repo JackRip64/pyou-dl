d7a271c677882c840bad55d179903f5cc0555345
JackRip64 <88751636+JackRip64@users.noreply.github.com>
2021-08-16 22:58:49 -0400

    Add files via upload
    Fixed issue with downloading music videos.
    "-- Audio" is automatically added in the search to remove unwanted videos.
    Improved use through use of official youtube_dl python library

--- pyou-dl.py
+++ pyou-dl.py
@@ -1,116 +1,77 @@
-
-### IMPORTANT ###
-# Do NOT use '/' in NEW_SONGS_FILE, it WILL break everything.
-# Im looking into a way to fix this as it is not a python problem
-# 	but a problem with how youtube-dl handles download names.
-# 
-# When typing songs, always leave out punctuation and capitalize
-# 	all words to minimize inconsistencies in the song dictionary.
-# You should also format the name so that it can be found at the
-# 	top of a single youtube search result. (Basically make it easy
-# 	to understand :P)
-# EXAMPLE: Thunderstruck -- ACDC
-
-import os
+#!/usr/bin/python3 
+import youtube_dl
 import pickle
-import json
 
-#constants
+# Global Vars
 NEW_SONGS_FILE = "songs_to_add.txt"
 USED_SONGS_FILE = "used_songs.pkl"
-DOWNLOAD_PATH = "../"
-CWD = os.getcwd()
+DOWNLOAD_PATH = ".."
 
-#variables
-used_songs = {}
 
-# Functions #
-#===================================================#
-
-#downloads new songs
-def add_new_songs():
-	
-	#loading in contents of USED_SONGS_FILE
+# adds new songs from NEW_SONGS_FILE
+def add_songs():
 	with open(USED_SONGS_FILE, "rb") as file:
 		used_songs = pickle.load(file)
 
-	#looping over every song in NEW_SONGS_FILE
 	with open(NEW_SONGS_FILE, "r") as file:
 		for song_name in [line.replace("\n", "") for line in file.readlines()]:
-			
-			#CONSOLE COMMANDS
-			#VVVVVVVVVVVVVVV#
-			INFO_COMMAND = f"youtube-dl ytsearch:'{song_name}' --no-playlist --write-info-json --skip-download -o '{song_name}'"
-			DOWNLOAD_COMMAND = f"youtube-dl -f bestaudio -x --audio-format 'mp3' -o '{DOWNLOAD_PATH}{song_name}.%(ext)s'"
 
-			#executing INFO_COMMAND to generate info.json
-			info_got = False
-			try:
-				os.system(INFO_COMMAND)
-				info_got = True
-			except Exception:
-				print(f"Could not download data for {song_name}")
+			# defining options for ytdl instance
+			DL_OPTIONS = {
+				'format': 'bestaudio',
+				'outtmpl': f'{DOWNLOAD_PATH}/{song_name}.%(ext)s',
+				'default_search': 'ytsearch',
+				'noplaylist': True,
+				'postprocessors': [{
+					'key': 'FFmpegExtractAudio',
+					'preferredcodec': 'mp3',
+					'preferredquality': '192'
+					}]
+			}
 
-			if info_got:
-				#finding info.json file
-				info_file = [file for file in os.listdir(CWD) if file.endswith("info.json")][0]
+			# pulling video info
+			with youtube_dl.YoutubeDL(DL_OPTIONS) as ytdl:
+				info = ytdl.extract_info(f"{song_name} -- Audio", download=False)["entries"][0]
 
-				#loading video_id from info.json file
-				video_id = json.load(open(info_file, "r"))["id"]
+				# pulling relevant info
+				video_title, video_id = info["title"], info["id"]
 
-				#deleting info.json file
-				os.system(f"rm '{info_file}'")
-
-				#check song_name and video_id against used_songs dict reject if in
+				# checking if video is already used
 				not_used = True
-				for name, video in used_songs.items():
-					if name == song_name:
-						print(f"\n[[[Song name '{song_name}' has already been used, not including...]]]\n")
+				for video in used_songs.values():
+					if video == video_id:
+						print(f"\n[[[Excluding {song_name}, ID already in use]]]\n")
 						not_used = False
 						break
-					elif video == video_id:
-						print(f"\n[[[Video ID '{video_id}' has already been used, not including...]]]\n")
-						not_used = False
-						break
-					else:
-						pass
 
-				#accepting entry into table for download
+				# accepting video into dict and downloading
 				if not_used:
 					used_songs[song_name] = video_id
-					os.system(f"{DOWNLOAD_COMMAND} youtube.com/watch?v={video_id}")
-					print(f"\n[[['{song_name}' has been successfully downloaded to '{DOWNLOAD_PATH}'!]]]\n")
-				else:
-					pass
-						
-	#clearing NEW_SONGS_FILE
-	with open(NEW_SONGS_FILE, "w") as file:
-		file.write("")
+					ytdl.download([f"youtube.com/watch?v={video_id}"])
+					print(f"\n[[['{song_name}' successfully downloaded]]]\n")
 
-	#dumping new used_songs into USED_SONGS_FILE
+	# dumping updated dict into USED_SONGS_FILE
 	with open(USED_SONGS_FILE, "wb") as file:
 		pickle.dump(used_songs, file)
 
-#shows used songs (who wouldve guessed?)
+# shows songs and id's from USED_SONGS_FILE
 def show_used_songs():
 	with open(USED_SONGS_FILE, "rb") as file:
-		used_songs = pickle.load(file)
-		for song_name, video_id in used_songs.items():
+		for song_name, video_id in pickle.load(file).items():
 			print(f"{song_name:>50} | {video_id}")
 
-#===================================================#
-# End Functions #
+# main function with decision making
+def main():
+	print("1. Add Songs\n2. View Used Songs")
+	response = None
+	while response not in ["1", "2"]:
+		response = input("> ")
+		if response not in ["1", "2"]:
+			print("Invalid Input")
+	if response == "1":
+		add_songs()
+	elif response == "2":
+		show_used_songs()
 
-#input for mode select
-answer = False
-while answer != "1" and answer != "2":
-	print("1. Add New Songs From File\n2. Display Used Songs")
-	answer = input("> ")
-
-#decision made
-if answer == "1":
-	add_new_songs()
-elif answer == "2":
-	show_used_songs()
-else:
-	pass
+if __name__ == '__main__':
+	main()
